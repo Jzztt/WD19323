a2c5ca2909aca8e48af31f5c609b5fa790e57000
Jettsyy <teazyyy2512@gmail.com>
2025-02-17 08:26:10 +0700

    feat: validate form register

--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,13 +1,37 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router";
+import { z } from "zod";
+
+const RegisterSchema = z.object({
+  name: z.string().min(1, "Name is required").max(255, "Name is too long"),
+  email: z.string().email("Email is invalid").min(1, "Email is required"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .max(255, "Password is too long"),
+});
+type RegisterSchemaType = z.infer<typeof RegisterSchema>;
 
 const Register = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<RegisterSchemaType>({
+    resolver: zodResolver(RegisterSchema),
+  });
+  const handleRegister = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
         <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
           <h2 className="text-2xl font-semibold text-center">Register</h2>
-          <form className="mt-4">
+          <form onSubmit={handleSubmit(handleRegister)} className="mt-4">
             <div className="mb-4">
               <label
                 className="block mb-2 text-sm font-bold text-gray-700"
@@ -16,10 +40,14 @@ const Register = () => {
                 Full Name
               </label>
               <input
+                {...register("name")}
                 type="text"
                 placeholder="Name"
                 className="w-full p-2 mt-2 border rounded"
               />
+              {errors.name && (
+                <span className="text-red-500"> {errors.name.message}</span>
+              )}
             </div>
             <div className="mb-4">
               <label
@@ -29,20 +57,31 @@ const Register = () => {
                 Email
               </label>
               <input
+                {...register("email")}
                 type="email"
                 placeholder="Email"
                 className="w-full p-2 mt-2 border rounded"
               />
+              {errors.email && (
+                <span className="text-red-500"> {errors.email.message}</span>
+              )}
             </div>
             <div className="mb-4">
-                <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="password">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  className="w-full p-2 mt-2 border rounded"
-                />
+              <label
+                className="block mb-2 text-sm font-bold text-gray-700"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                {...register("password")}
+                type="password"
+                placeholder="Password"
+                className="w-full p-2 mt-2 border rounded"
+              />
+              {errors.password && (
+                <span className="text-red-500"> {errors.password.message}</span>
+              )}
             </div>
             <button className="w-full p-2 mt-4 text-white bg-green-500 rounded">
               Register
